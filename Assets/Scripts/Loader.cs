15cdaf93335ad6a881521ba04198e2ede117f93b
BoberUnity <boberberezin@gmail.com>
2025-03-05 09:43:44 +0300

    Удалены лишние ассеты

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
 using System.IO;
 using UnityEngine;
-//using static System.Net.Mime.MediaTypeNames;
 
 public class Loader : MonoBehaviour
 {
-    public string assetName = "Scene";
-    public string bundleName = "testbundle";
-    
-    void Start()
+    private string assetName = "Scene";
+    private string bundleName = "testbundle";
+
+    private void Start()
     {
         AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
 
